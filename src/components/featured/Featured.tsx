f7a80690acfb9cd374b4d2f33407b3eb991d766a
Aloseous Kingsly <50615352+Aloseous@users.noreply.github.com>
2024-09-25 20:59:23 +0530

    lint fix

--- src/components/featured/Featured.tsx
+++ src/components/featured/Featured.tsx
@@ -11,7 +11,7 @@ const Featured = () => {
         </div>
         <div className={styles.textContainer}>
           <h1 className={styles.postTitle}>Your Guide to Global Adventures</h1>
-          <p className={styles.postDesc}>Welcome to Discover the World, a blog dedicated to exploring the beauty and diversity of our planet. From bustling cities to serene landscapes, we take you on a journey through breathtaking destinations, rich cultures, and unforgettable experiences. Whether you're planning your next vacation, seeking travel inspiration, or simply curious about the world around you, our blog offers insightful articles, practical tips, and personal stories to ignite your wanderlust. Join us as we uncover hidden gems, share travel hacks, and celebrate the wonders of global exploration, one adventure at a time!</p>
+          <p className={styles.postDesc}>Welcome to Discover the World, a blog dedicated to exploring the beauty and diversity of our planet. From bustling cities to serene landscapes, we take you on a journey through breathtaking destinations, rich cultures, and unforgettable experiences. Whether you are planning your next vacation, seeking travel inspiration, or simply curious about the world around you, our blog offers insightful articles, practical tips, and personal stories to ignite your wanderlust. Join us as we uncover hidden gems, share travel hacks, and celebrate the wonders of global exploration, one adventure at a time!</p>
           {/* <button className={styles.button}>Read More</button> */}
         </div>
       </div>
